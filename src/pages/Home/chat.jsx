c9c9d9ffe7569a8e1dc8ebfa7340892515e555c9
kunalt <kunaltakalkar.123@gmail.com>
2022-02-08 14:39:56 +0530

    vid 8.2

--- src/pages/Home/chat.jsx
+++ src/pages/Home/chat.jsx
@@ -4,35 +4,42 @@ import { Loader } from 'rsuite';
 import Top from '../../components/chat-window/chat-top';
 import Bottom from '../../components/chat-window/chatBottom';
 import Messages from '../../components/chat-window/messages';
+import { CurrentRoomProvider } from '../../context/current.room.context';
 import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
+  const { chatId } = useParams();
+  const rooms = useRooms();
 
-  const { chatId } = useParams()
-  const rooms = useRooms()
-
-  if(!rooms ){
-    return <Loader center vertical size="md" content="Loading" speed='slow' />
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
   }
 
-  const currentRoom = rooms.find(room => room.id === chatId)
+  const currentRoom = rooms.find(room => room.id === chatId);
 
-  if(!currentRoom){
-    return <h6 className='text-center mt-page'> Chat {chatId} not found.</h6>
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page"> Chat {chatId} not found.</h6>;
   }
+
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
 
   return (
-    <>
-      <div className='chat-top'>
+    <CurrentRoomProvider data={currentRoomData}>
+      <div className="chat-top">
         <Top />
       </div>
-      <div className='chat-middle'>
+      <div className="chat-middle">
         <Messages />
       </div>
-      <div className='chat-bottom'>
+      <div className="chat-bottom">
         <Bottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
