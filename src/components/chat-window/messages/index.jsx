4b0ffc1a2fbe5e921c8495eaf37beecff6be1795
kunalt <kunaltakalkar.123@gmail.com>
2022-02-09 14:24:11 +0530

    displayed messages on chat page video 10.1

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/Firebase';
+import { transformtoArrayWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
-  return <div>message</div>;
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messageRef = database.ref('/messages');
+
+    messageRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformtoArrayWithId(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messageRef.off('value');
+    };
+  }, [chatId]);
+
+  return <ul className='msg-list custom-scroll'>
+
+    {
+      isChatEmpty && <li>No Messages Yet</li>
+    }
+    {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg} /> )}
+
+
+  </ul>;
 };
 
 export default Messages;
